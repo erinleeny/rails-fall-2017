dcd2f4dac8a19732a217ab2f4a7fa779cc8702a9
erinl1 <erin.lee.ny@gmail.com>
2017-09-15 18:58:31 -0400

    Saved

--- calc/app/controllers/calculator_controller.rb
+++ calc/app/controllers/calculator_controller.rb
@@ -6,8 +6,12 @@ class CalculatorController < ApplicationController
     elsif params[:dropdown] == "-"
       @answer = params[:x].to_i - params[:y].to_i
     elsif params[:dropdown] == "/"
-      @answer = params[:x].to_f/params[:y].to_f
-    elsif params[:dropdown] == "X"
+      if params[:x].to_i == 0 || params[:y].to_i == 0
+        @answer = 'undefined'
+      elsif
+        @answer = params[:x].to_f/params[:y].to_f
+      end
+    elsif params[:dropdown] == "*"
       @answer = params[:x].to_i*params[:y].to_i
     elsif params[:dropdown] == "^"
       @answer = params[:x].to_i**params[:y].to_i

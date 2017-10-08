9e550dbf8d52b8343b0e3ccb09e481d6db1fa591
erinl1 <erin.lee.ny@gmail.com>
2017-10-08 15:45:50 -0400

    Edits

--- rescue/config/routes.rb
+++ rescue/config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # =>   action       controller to access
-  get "/animals", to: "animals#index"
-  get "/animals/new", to: "animals#new"
-  get "/animals/:id", to: "animals#show"
-  post "/animals", to: "animals#create"
-
+#  get "/animals", to: "animals#index"
+#  get "/animals/new", to: "animals#new"
+#  get "/animals/:id", to: "animals#show"
+#  post "/animals", to: "animals#create"
+resources :animals, only: [:index, :show]
+resources :animals do
+  collection do
+    get 'list'
+  end
+end
+ #resources :animals, except: [:create, :new]
 end

d7e94cc01c2c3292b0fbaf207f0f6d3f9164d195
Erin Lee <erin.lee.ny@gmail.com>
2019-03-06 23:20:37 -0500

    finished

--- rails-converter/app/controllers/converter_controller.rb
+++ rails-converter/app/controllers/converter_controller.rb
@@ -2,9 +2,20 @@ class ConverterController < ApplicationController
   def index
     @first_c = params[:first]
     @second_c = params[:second]
-    @num = params[:num].to_i
+    @num = params[:num].to_f
     if @first_c == 'c' && @second_c == 'f'
-      @num = @num * 9 / 5 + 32
+      @newnum = @num * 9 / 5 + 32
+    elsif @first_c == 'f' && @second_c == 'c'
+      @newnum = ((@num - 32) * 5 / 9).round
+      puts @newnum
+    elsif @first_c == 'k' && @second_c == 'c'
+      @newnum = (@num - 273.15).round
+    elsif @first_c == 'c' && @second_c == 'k'
+      @newnum = (@num + 273.15).round
+    elsif @first_c == 'k' && @second_c == 'f'
+      @newnum = ((@num - 273.15) * 9 / 5 + 32).round
+    elsif @first_c == 'f' && @second_c == 'k'
+      @newnum = (((@num - 32) * 5 / 9) + 273.15).round
     end
   end
 end
